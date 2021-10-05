2e31ed4ff8a84077a65f62d09d8ff083864394ad
wangjunm <529474558@qq.com>
2021-10-05 15:28:43 +0800

    sina

--- sina_status.js
+++ sina_status.js
@@ -8,7 +8,7 @@ cron "30 8,12,15,18,21 * * *" jd_CheckCK.js, tag:京东CK检测by-ccwav
 //增加变量停用自动启用CK:  export CKAUTOENABLE="false"
 //增加变量不显示CK备注:  export CKREMARK="false"
 const $ = new Env('微博统计');
-const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = require('./sendNotify');
 //Node.js用户请在jdCookie.js处填写京东ck;
 var mysql      = require('mysql');
 
