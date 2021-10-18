7986efdc20febad70fedf58bc4aadefbd9078be3
wangjunm <529474558@qq.com>
2021-10-18 22:13:56 +0800

    10.18

--- jd_cfd_mooncake.js
+++ jd_cfd_mooncake.js
@@ -647,12 +647,6 @@ function readShareCode() {
 function shareCodesFormat() {
   return new Promise(async resolve => {
     $.newShareCodes = []
-    const readShareCodeRes = null ; //await readShareCode();
-    if (readShareCodeRes && readShareCodeRes.code === 200) {
-      $.newShareCodes = [...new Set([...$.shareCodes, ...$.strMyShareIds, ...(readShareCodeRes.data || [])])];
-    } else {
-      $.newShareCodes = [...new Set([...$.shareCodes, ...$.strMyShareIds])];
-    }
     console.log(`您将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
