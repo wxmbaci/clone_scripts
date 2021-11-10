060dc64aaef28d710edd090fc42be10dc0a6985a
wangjunm <529474558@qq.com>
2021-11-10 09:20:50 +0800

    1109

--- jd_fruit.js
+++ jd_fruit.js
@@ -1316,9 +1316,6 @@ function shareCodesFormat() {
     if ($.shareCodesArr[$.index - 1]) {
       newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-      const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-      newShareCodes = shareCodes[tempIndex].split('@');
     }
     const readShareCodeRes =null;
     if (readShareCodeRes && readShareCodeRes.code === 200) {
